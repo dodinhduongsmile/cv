0f4c0f3464f4f614b6c6818837a9476067993f16
dodinhduongsmile <dodinhduongsmile@gmail.com>
2020-03-07 12:06:26 +0700

    trang ca nhan

---
+++ js/index.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// cho menu chạy từ từ xuống
+    $('.navbar-collapse').find('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 40)
+                }, 1000);
+                if ($('.navbar-toggle').css('display') != 'none') {
+                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
+                }
+                return false;
+            }
+        }
+    });
+// scroll Up, back totop
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 600) {
+			$('.totop').fadeIn('slow');// hiển thị nút totop rõ dần khi top>600px
+		} else {
+			$('.totop').fadeOut('slow'); // mờ dần đi khi top<600px
+		}
+	});
+
+	$('.totop').click(function () {
+		$("html, body").animate({scrollTop: 0}, 1000);// cho chạy chậm 1000milis khi ấn totop
+		return false;
+	});
+
+// main-menu-scroll - menu chạy theo, cố định
+jQuery(window).scroll(function () {
+var top = jQuery(document).scrollTop();
+var height = 300;
+//alert(batas);
+
+if (top > height) {
+	jQuery('.header').addClass('menu-scroll');
+} else {
+	jQuery('.header').removeClass('menu-scroll');
+}
+});
